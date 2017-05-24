910d9bfc45b1ee421ffe4fe13844863d9b01e574
Andy Mayshar <andy_mayshar@idoc-uat-ct1.ouvwakuy3dletgb0rklzn4uzrh.ax.internal.usgovcloudapp.net>
2017-05-24 17:57:15 +0000

    change auth site

--- providers/azure.go
+++ providers/azure.go
@@ -21,7 +21,7 @@ func NewAzureProvider(p *ProviderData) *AzureProvider {
 	if p.ProfileURL == nil || p.ProfileURL.String() == "" {
 		p.ProfileURL = &url.URL{
 			Scheme:   "https",
-			Host:     "idoco360train.api.crm9.dynamics.com",
+			Host:     "login.windows.net",
 			Path:     "/me",
 			RawQuery: "api-version=1.6",
 
